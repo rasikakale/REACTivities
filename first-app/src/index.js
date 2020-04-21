c5b12f66a94401960f2f26fd12a3618fae215697
Rasika Kale <rkale@ucsc.edu>
2020-04-20 19:00:03 -0700

    created new react app

---
+++ first-app/src/index.js
@@ -0,0 +1,9 @@
+// import the react and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+// Create a react component 
+
+
+// Take the react componant and show it on the screen
