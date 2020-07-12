8d8a9e593a8b930797ec6b7c92be2699bc4c5bc0
Rasika Kale <rkale@ucsc.edu>
2020-07-12 12:13:08 -0700

    Created sample blog post comments

--- components/src/index.js
+++ components/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+    return (
+        <div className="ui containter comments">
+          <CommentDetail author="Sam" timeAgo="Today at 4:45PM" response="Nice blog post!" avatar={faker.image.avatar()}/>
+          <CommentDetail author="Alex" timeAgo="Today at 2:00AM" response="This was alright..." avatar={faker.image.avatar()}/>
+          <CommentDetail author="Jane" timeAgo="Yesterday at 6:00PM" response="Love the creativity! :)" avatar={faker.image.avatar()}/>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
