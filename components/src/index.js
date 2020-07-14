fd6cfe17292aeb8c710e4b2e2ba0b9159cd903dc
Rasika Kale <rasikakale@gmail.com>
2020-07-13 19:18:15 -0700

    added ui card for sample blog post

--- components/src/index.js
+++ components/src/index.js
@@ -2,14 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 
 const App = () => {
     return (
         <div className="ui containter comments">
-          <CommentDetail author="Sam" timeAgo="Today at 4:45PM" response="Nice blog post!" avatar={faker.image.avatar()}/>
-          <CommentDetail author="Alex" timeAgo="Today at 2:00AM" response="This was alright..." avatar={faker.image.avatar()}/>
-          <CommentDetail author="Jane" timeAgo="Yesterday at 6:00PM" response="Love the creativity! :)" avatar={faker.image.avatar()}/>
+          <ApprovalCard>
+            <div>
+              <h4> Warning!</h4>
+              Are you sure you want to do this?
+            </div>
+            
+          </ApprovalCard>
+          <ApprovalCard> 
+            <CommentDetail 
+              author="Sam" 
+              timeAgo="Today at 4:45PM" 
+              response="Nice blog post!" 
+              avatar={faker.image.avatar()}
+            />
+          </ApprovalCard>
+
+          <ApprovalCard>
+            <CommentDetail 
+              author="Alex" 
+              timeAgo="Today at 2:00AM" 
+              response="This was alright..." 
+              avatar={faker.image.avatar()}
+            />
+           </ApprovalCard>
+
+          <ApprovalCard>
+            <CommentDetail 
+              author="Jane"  
+              timeAgo="Yesterday at 6:00PM" 
+              response="Love the creativity! :)" 
+              avatar={faker.image.avatar()}
+            />
+          </ApprovalCard>
         </div>
     );
 };
