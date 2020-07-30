bb410ec4ed5b3aba2ae8bd826c460a4f154d6540
Rasika Kale <rasikakale@gmail.com>
2020-07-29 23:24:23 -0700

    added list of images for anything user searches

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,8 +1,12 @@
+import './ImageList.css';
 import React from 'react';
 
 const ImageList = (props) => {
-    console.log(props.images);
-    return <div>ImageList</div>
+    //console.log(props.images);
+    const images = props.images.map(({description, id, urls}) => {
+        return <img alt={description} key={id} src={urls.regular} />
+    });
+    return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
