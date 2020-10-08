b1db86f67b717b974af3a28ea2d6a9237dc3f866
Rasika Kale <rasikakale@gmail.com>
2020-10-07 22:30:00 -0700

    Completed Sample Youtube page using Youtube API

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -6,7 +6,11 @@ const VideoItem = ({video, onVideoSelect}) => {
     return (
         //use callback function to pass a property back to parent (app.js) class and show to user
         <div onClick ={() => onVideoSelect(video)} className="video-item item">
-            <img className="ui image" src={video.snippet.thumbnails.medium.url}/>
+            <img 
+                alt={video.snippet.title} 
+                className="ui image" 
+                src={video.snippet.thumbnails.medium.url}
+            />
             <div className="content"> 
                 <div className="header">{video.snippet.title}</div>
             </div>
