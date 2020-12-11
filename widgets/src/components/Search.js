0cc04d432de7411d1060225e6609d4d3d1fe14f1
Rasika Kale <rasikakale@gmail.com>
2020-12-10 23:19:50 -0800

    Created Dropdown using hooks in react

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -5,9 +5,42 @@ import axios from 'axios';
 const Search = () => {
     const [term, setTerm] = useState('programming');
 
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
+
     const [results, setResults] = useState([]);
 
-    //console.log(results);
+    // runs anytime term changes
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [term]);
+
+    // only run when first component is rendered or when debounced term is changed
+    useEffect(() => {
+        const search = async () => {
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    origin: '*',
+                    list: 'search',
+                    format: 'json',
+                    srsearch: debouncedTerm,
+                },
+            });
+
+            setResults(data.query.search);
+        };
+        if(debouncedTerm) {
+            search();
+        }
+        
+    }, [debouncedTerm]);
+   
     
     useEffect(() => {
         // console.log("initial render or term was changed");
@@ -15,6 +48,7 @@ const Search = () => {
         // return () => {
         //     console.log("cleanup");
         // };
+        /*
         const search = async () => {
             const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
@@ -28,16 +62,28 @@ const Search = () => {
 
             setResults(data.query.search);
         };
+        */
 
-        const timeoutId = setTimeout(() => {
-            if (term) {
-                search();
-            }
-        }, 500);
+        // searching on initial render
+        /*
+        if (term && !results.length) {
+            search();
+        } else {
+            const timeoutId = setTimeout(() => {
+                if (term) {
+                    search();
+                }
+            }, 1000);
+            
+            return () => {
+                clearTimeout(timeoutId);
+            };
+        }
+        */
+
         
-       
-        
-    }, [term]);
+        // dependency array
+    }, [term, results.length]);
 
     const renderedResults = results.map((result) => {
         return (
